480c3b5f1a5b133b8720281319bfb4b4fe236138
Flameshock340 <gcrovetto2021@fau.edu>
2024-11-14 20:35:17 -0500

    Initial Commit

---
+++ EncourageApp/EncourageApp/EncourageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncourageAppApp.swift
+//  EncourageApp
+//
+//  Created by Giannfranco Crovetto on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct EncourageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
