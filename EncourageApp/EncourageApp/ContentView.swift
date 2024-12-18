417bdc65489b565eb65b36f9b1dfd8941643fb96
Flameshock340 <gcrovetto2021@fau.edu>
2024-12-17 22:17:23 -0500

    Added rounded rectangles to represent as categories.

--- EncourageApp/EncourageApp/ContentView.swift
+++ EncourageApp/EncourageApp/ContentView.swift
@@ -8,24 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var authManager: AuthManager // Access the authManager
+    
     var body: some View {
-        VStack(spacing: 20) {
-            // Title
-            Text("Encourage App")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.top)
-            
-            Spacer()
-            
-            Text("Hello there")
-            
-            // Footer
-            Text("Created by: Giannfranco Crovetto")
-                .font(.footnote)
-                .padding()
+        NavigationView {
+            VStack(spacing: 20) {
+                // Title
+                Text("Encourage App")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.bold)
+                    .padding(.top)
+                
+                // Scrollable body
+                ScrollView(.vertical) {
+                    // Rounded rectangle
+                    VStack(spacing: 14) {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(.systemGray6)) // Light gray background
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                            .frame(width: 350, height: 100) // Adjust the width and height
+                            .padding(.horizontal)
+                        
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(.systemGray6)) // Light gray background
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                            .frame(width: 350, height: 100) // Adjust the width and height
+                            .padding(.horizontal)
+                        
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(.systemGray6)) // Light gray background
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                            .frame(width: 350, height: 100) // Adjust the width and height
+                            .padding(.horizontal)
+                        
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(.systemGray6)) // Light gray background
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                            .frame(width: 350, height: 100) // Adjust the width and height
+                            .padding(.horizontal)
+                        
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(.systemGray6)) // Light gray background
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                            .frame(width: 350, height: 100) // Adjust the width and height
+                            .padding(.horizontal)
+                        
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(.systemGray6)) // Light gray background
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                            .frame(width: 350, height: 100) // Adjust the width and height
+                            .padding(.horizontal)
+                        
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(.systemGray6)) // Light gray background
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                            .frame(width: 350, height: 100) // Adjust the width and height
+                            .padding(.horizontal)
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
