6e7604b50ee60f7420ff71129e92c2560083afd0
Flameshock340 <gcrovetto2021@fau.edu>
2024-12-18 15:15:51 -0500

    Added labels for each category.

--- EncourageApp/EncourageApp/ContentView.swift
+++ EncourageApp/EncourageApp/ContentView.swift
@@ -22,49 +22,94 @@ struct ContentView: View {
                 
                 // Scrollable body
                 ScrollView(.vertical) {
-                    // Rounded rectangle
+                    // Rounded rectangles
                     VStack(spacing: 14) {
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color(.systemGray6)) // Light gray background
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
-                            .frame(width: 350, height: 100) // Adjust the width and height
-                            .padding(.horizontal)
+                        // Category 1
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(.systemGray6)) // Light gray background
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                                .frame(width: 350, height: 100) // Adjust the width and height
+                                .padding(.horizontal)
+                            Text("Self Motivation")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                        }
                         
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color(.systemGray6)) // Light gray background
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
-                            .frame(width: 350, height: 100) // Adjust the width and height
-                            .padding(.horizontal)
+                        // Category 2
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(.systemGray6)) // Light gray background
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                                .frame(width: 350, height: 100) // Adjust the width and height
+                                .padding(.horizontal)
+                            Text("Mindfulness")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                        }
                         
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color(.systemGray6)) // Light gray background
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
-                            .frame(width: 350, height: 100) // Adjust the width and height
-                            .padding(.horizontal)
+                        // Category 3
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(.systemGray6)) // Light gray background
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                                .frame(width: 350, height: 100) // Adjust the width and height
+                                .padding(.horizontal)
+                            Text("Anxiety")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                        }
                         
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color(.systemGray6)) // Light gray background
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
-                            .frame(width: 350, height: 100) // Adjust the width and height
-                            .padding(.horizontal)
+                        // Category 4
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(.systemGray6)) // Light gray background
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                                .frame(width: 350, height: 100) // Adjust the width and height
+                                .padding(.horizontal)
+                            Text("Depression")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                        }
                         
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color(.systemGray6)) // Light gray background
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
-                            .frame(width: 350, height: 100) // Adjust the width and height
-                            .padding(.horizontal)
+                        // Category 5
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(.systemGray6)) // Light gray background
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                                .frame(width: 350, height: 100) // Adjust the width and height
+                                .padding(.horizontal)
+                            Text("Uplifting")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                        }
                         
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color(.systemGray6)) // Light gray background
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
-                            .frame(width: 350, height: 100) // Adjust the width and height
-                            .padding(.horizontal)
+                        // Category 6
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(.systemGray6)) // Light gray background
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                                .frame(width: 350, height: 100) // Adjust the width and height
+                                .padding(.horizontal)
+                            Text("Stage Fright")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                        }
                         
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color(.systemGray6)) // Light gray background
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
-                            .frame(width: 350, height: 100) // Adjust the width and height
-                            .padding(.horizontal)
+                        // Category 7
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(.systemGray6)) // Light gray background
+                                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5) // Creates a shadow underlay
+                                .frame(width: 350, height: 100) // Adjust the width and height
+                                .padding(.horizontal)
+                            Text("Mindfulness")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                        }
+                        
+                        // Add more categories here...
+                        
                     }
                 }
             }
